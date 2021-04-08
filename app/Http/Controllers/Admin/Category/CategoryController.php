71397fd827a20c4ff531acccb846f90fda6d48af
mimshe <MSAHacker12>
2021-04-08 16:17:30 +0430

    delete bug fixed!

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -16,56 +16,65 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    use EditCategoryTrait,CreateCategoryTrait;
+    use EditCategoryTrait, CreateCategoryTrait;
+
     public function index()
     {
         return adminView("pages.admin.category.index");
+    }
 
+    function getChildrenCategories($categoryId)
+    {
+        $categories = Category::whereParentId($categoryId)->get();
+        foreach ($categories as $category) {
+            $category->children = $this->getChildrenCategories($category->id);
+        }
+        return $categories;
     }
 
     public function list()
     {
-        $categories=Category::paginate(12);
+        $categories = Category::whereParentId(0)->get();
+        foreach ($categories as $category) {
+            $category->childern = $this->getChildrenCategories($category->id);
+        }
         return $categories;
-
     }
 
-    public function store(CreateCategoryRequest $request){
+    public function store(CreateCategoryRequest $request)
+    {
 
 
-        $category=$this->createCategory(
+        $category = $this->createCategory(
             [
-                'name'=>$request->input('name'),
-                'slug'=>$request->input('slug'),
-                'image'=>$request->image,
-                'content'=>$request->input('content'),
-                'fields'=>$request->input('fields'),
-                'parent_id'=>$request->input('parent_id'),
-                'layout_id'=>$request->input('layout_id'),
-                'module_id'=>$request->input('module_id'),
-                'status'=>$request->input('status'),
-                'is_menu'=>$request->input('is_menu'),
+                'name' => $request->input('name'),
+                'slug' => $request->input('slug'),
+                'image' => $request->image,
+                'content' => $request->input('content'),
+                'fields' => $request->input('fields'),
+                'parent_id' => $request->input('parent_id'),
+                'layout_id' => $request->input('layout_id'),
+                'module_id' => $request->input('module_id'),
+                'status' => $request->input('status'),
+                'is_menu' => $request->input('is_menu'),
 
             ]
         );
 
 
         if ($request->input('tag_list'))
-            foreach ($request->input('tag_list') as $tagName){
-                $tag=Tag::firstOrCreate([
-                    'name'=>$tagName
+            foreach ($request->input('tag_list') as $tagName) {
+                $tag = Tag::firstOrCreate([
+                    'name' => $tagName
                 ]);
                 CategoryTag::firstOrCreate([
-                    'tag_id'=>$tag->id,
-                    'category_id'=>$category->id,
+                    'tag_id' => $tag->id,
+                    'category_id' => $category->id,
                 ]);
             }
 
 
-
-
         return redirect(route("admin.category.index"))->with("info", "ثبت دسته بندی با موفقیت انجام شد");
-
 
 
     }
@@ -77,8 +86,8 @@ class CategoryController extends Controller
 
     public function edit($categoryId)
     {
-        $category=Category::findOrFail($categoryId);
-        return adminView("pages.admin.category.edit")->with("category",$category);
+        $category = Category::findOrFail($categoryId);
+        return adminView("pages.admin.category.edit")->with("category", $category);
     }
 
     public function destroy($id)
@@ -90,59 +99,56 @@ class CategoryController extends Controller
     public function multipleDestroy(multipleDestroyRequest $request)
     {
         if (isset($request->categoryIds))
-            Category::whereIn('id',$request->input('categoryIds'))->delete();
+            Category::whereIn('id', $request->input('categoryIds'))->delete();
 
-        return redirect(route("admin.category.index"))->with('info','دسته بندی های انتخاب شده حذف شدند');
+        return redirect(route("admin.category.index"))->with('info', 'دسته بندی های انتخاب شده حذف شدند');
 
     }
 
 
-
-    public function update(EditCategoryRequest $request,$categoryId)
+    public function update(EditCategoryRequest $request, $categoryId)
     {
-        $category=$this->EditCategory([
-            'name'=>$request->input('name'),
-            'slug'=>$request->input('slug'),
-            'image'=>$request->input('image'),
-            'content'=>$request->input('content'),
-            'fields'=>$request->input('fields'),
-            'parent_id'=>$request->input('parent_id'),
-            'layout_id'=>$request->input('layout_id'),
-            'module_id'=>$request->input('module_id'),
-            'status'=>$request->input('status'),
-            'category_id'=>$categoryId,
-            'is_menu'=>$request->input('is_menu'),
-
+        $category = $this->EditCategory([
+            'name' => $request->input('name'),
+            'slug' => $request->input('slug'),
+            'image' => $request->input('image'),
+            'content' => $request->input('content'),
+            'fields' => $request->input('fields'),
+            'parent_id' => $request->input('parent_id'),
+            'layout_id' => $request->input('layout_id'),
+            'module_id' => $request->input('module_id'),
+            'status' => $request->input('status'),
+            'category_id' => $categoryId,
+            'is_menu' => $request->input('is_menu'),
 
 
         ]);
 
         if ($request->input('tag_list'))
-            foreach ($request->input('tag_list') as $tagName){
-                $tag=Tag::firstOrCreate([
-                    'name'=>$tagName
+            foreach ($request->input('tag_list') as $tagName) {
+                $tag = Tag::firstOrCreate([
+                    'name' => $tagName
                 ]);
                 CategoryTag::firstOrCreate([
-                    'tag_id'=>$tag->id,
-                    'category_id'=>$categoryId,
+                    'tag_id' => $tag->id,
+                    'category_id' => $categoryId,
                 ]);
             }
 
-        return redirect(route("admin.category.edit",$categoryId))->with("info", "عملیات ویرایش دسته بندی با موفقیت انجام شد");
+        return redirect(route("admin.category.edit", $categoryId))->with("info", "عملیات ویرایش دسته بندی با موفقیت انجام شد");
 
 
     }
 
-    public function search(SearchCategoryRequest $request){
-        $searchHelper=new \CategorySearchHelper($request);
-        $categories=$searchHelper->searchCategories();
-        $categories=$searchHelper->statusCategory($categories);
+    public function search(SearchCategoryRequest $request)
+    {
+        $searchHelper = new \CategorySearchHelper($request);
+        $categories = $searchHelper->searchCategories();
+        $categories = $searchHelper->statusCategory($categories);
 
-        return adminView("pages.admin.category.index")->with('categories',$categories);
+        return adminView("pages.admin.category.index")->with('categories', $categories);
 
     }
-
-
 
 
 }
