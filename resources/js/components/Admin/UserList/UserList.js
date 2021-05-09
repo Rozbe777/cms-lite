0356b880f5be811a8a65e4e452c40cce27fc6445
unknown <emran.rastadi@gmail.com>
2021-05-09 17:55:48 +0430

    update user list

--- resources/js/components/Admin/UserList/UserList.js
+++ resources/js/components/Admin/UserList/UserList.js
@@ -6,6 +6,7 @@ import {Pagination} from './../_Micro/Pagination';
 import {UserColumns} from './../_Micro/TableColumnsList'
 import {DeleteGroupt} from './../_Shared/java';
 import $ from 'jquery';
+import SearchComponent from "./Search";
 
 const UserList = memo((props) => {
     const {token} = props;
@@ -182,144 +183,103 @@ const UserList = memo((props) => {
     }
 
     return (
-        <form id="myForm">
-            <div className="heading-layout1">
-                <div className="item-title" style={{display: "none"}}>
-                    <h3>لیست کاربران</h3>
-                </div>
-                <div className="dropdown" style={{display: "none"}}>
-                    <a className="dropdown-toggle" role="button"
-                       data-toggle="dropdown" aria-expanded="false">مدیریت</a>
-                    <div className="dropdown-menu dropdown-menu-right">
-                        <a className="dropdown-item sweet-alert-multi-delete-confirm">
-                            <i className="bx bx-trash mr-1"></i> حذف</a>
+
+
+        <>
+            <SearchComponent />
+            <form id="myForm">
+                <div className="heading-layout1">
+                    <div className="item-title" style={{display: "none"}}>
+                        <h3>لیست کاربران</h3>
                     </div>
-                </div>
-            </div>
-
-            <div className="users-list-table">
-                <form>
-                    <div className="users-list-filter px-1">
-                        <div className="row border rounded py-2 mb-2" id={"header-card-custom"}>
-                            <div className="col-12 col-sm-6 col-lg-3">
-                                <label htmlFor="users-list-verified">جستجو</label>
-                                <input type="text" className="form-control"
-                                       placeholder="جستجو با ایمیل و تلفن ..." name="search"/>
-
-                            </div>
-
-                            <div className="col-12 col-sm-6 col-lg-2">
-                                <label htmlFor="users-list-verified">تایید شده</label>
-                                <fieldset className="form-group">
-                                    <select className="form-control" id="users-list-verified" name="confirmed">
-                                        <option>همه</option>
-                                        <option value="1">بله</option>
-                                        <option value="0">خیر</option>
-                                    </select>
-                                </fieldset>
-                            </div>
-                            <div className="col-12 col-sm-6 col-lg-2">
-                                <label htmlFor="users-list-role">نقش</label>
-                                <fieldset className="form-group">
-                                    <select className="form-control" id="users-list-role" name="role">
-                                        <option>همه</option>
-                                        <option>مدیر</option>
-                                        <option>کاربر</option>
-                                    </select>
-                                </fieldset>
-                            </div>
-                            <div className="col-12 col-sm-6 col-lg-2">
-                                <label htmlFor="users-list-status">وضعیت</label>
-                                <fieldset className="form-group">
-                                    <select className="form-control" id="users-list-status" name="status">
-                                        <option>همه</option>
-                                        <option value="active">فعال</option>
-                                        <option value="deactivate">غیر فعال</option>
-                                    </select>
-                                </fieldset>
-                            </div>
-                            <div className="col-6 col-sm-3 col-lg-1 " style={{marginBlockStart: 'auto'}}>
-                                <button type="submit" className="btn btn-primary mr-1 mb-1">جستجو</button>
-                            </div>
-                            <div className="col-6 col-sm-3 col-lg-1 " style={{marginBlockStart: 'auto'}}>
-                                <a className="btn btn-icon rounded-circle btn-warning mr-1 mb-1 tui-full-calendar-dayname-leftmargin"
-                                   href={props.exportlink}
-                                   style={{marginRight: '20px'}} title="خروجی اکسل">
-                                    <i className="bx bx-archive"></i></a>
-                            </div>
+                    <div className="dropdown" style={{display: "none"}}>
+                        <a className="dropdown-toggle" role="button"
+                           data-toggle="dropdown" aria-expanded="false">مدیریت</a>
+                        <div className="dropdown-menu dropdown-menu-right">
+                            <a className="dropdown-item sweet-alert-multi-delete-confirm">
+                                <i className="bx bx-trash mr-1"></i> حذف</a>
                         </div>
                     </div>
-                </form>
-                <div className="card">
-                    <div className="card-content">
-                        <div className="card-body" style={{padding: '0px'}}>
-                            <div className="table-responsive">
-                                <table id="users-list-datatable" className="table table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th>
-                                            <div className={"form-check"}>
+                </div>
 
-                                                <div id={"edit-boxes"}>
-                                                    <a className="dropdown-item" onClick={e => handleDeleteGroup(e)}
-                                                       style={{cursor: 'pointer'}}>
-                                                        <i style={{float: 'right'}}
-                                                           className="bx bx-trash mr-1"></i> حذف گروهی</a>
+
+
+
+
+                <div className="users-list-table">
+
+                    <div className="card">
+                        <div className="card-content">
+                            <div className="card-body" style={{padding: '0px'}}>
+                                <div className="table-responsive">
+                                    <table id="users-list-datatable" className="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>
+                                                <div className={"form-check"}>
+
+                                                    <div id={"edit-boxes"}>
+                                                        <a className="dropdown-item" onClick={e => handleDeleteGroup(e)}
+                                                           style={{cursor: 'pointer'}}>
+                                                            <i style={{float: 'right'}}
+                                                               className="bx bx-trash mr-1"></i> حذف گروهی</a>
+                                                    </div>
+
+                                                    <input type="checkbox"
+                                                           id={"checkAll"}
+                                                           onClick={() => allUser ? addAll(allUser) : ''}
+                                                           className="form-check-input "/>
+                                                    <label className="form-check-label"></label>
                                                 </div>
 
-                                                <input type="checkbox"
-                                                       id={"checkAll"}
-                                                       onClick={() => allUser ? addAll(allUser) : ''}
-                                                       className="form-check-input "/>
-                                                <label className="form-check-label"></label>
-                                            </div>
+                                            </th>
+                                            <th>ID</th>
+                                            <th>کاربر</th>
+                                            <th>ایمیل</th>
+                                            <th>شماره موبایل</th>
+                                            <th>نقش</th>
+                                            <th>وضعیت</th>
+                                            <th>عملیات</th>
 
-                                        </th>
-                                        <th>ID</th>
-                                        <th>کاربر</th>
-                                        <th>ایمیل</th>
-                                        <th>شماره موبایل</th>
-                                        <th>نقش</th>
-                                        <th>وضعیت</th>
-                                        <th>عملیات</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <UserColumns oldUserId={userId.userIds} loading={loading}
+                                                     data={currentUsers}
+                                                     userid={item => {
+                                                         selectHandler(item)
+                                                     }}
+                                                     pushPopFade={stat => stat ? $("#edit-boxes").fadeIn(0) : $("#edit-boxes").fadeOut(0)}
 
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <UserColumns oldUserId={userId.userIds} loading={loading}
-                                                 data={currentUsers}
-                                                 userid={item => {
-                                                     selectHandler(item)
-                                                 }}
-                                                 pushPopFade={stat => stat ? $("#edit-boxes").fadeIn(0) : $("#edit-boxes").fadeOut(0)}
-
-                                    />
-
-                                    </tbody>
-                                </table>
-                                <div className="col-md-12">
-                                    {userData ? (
-                                        <Pagination
-                                            firstPageUrl={userData.first_page_url}
-                                            lastPageUrl={userData.last_page_url}
-                                            currentPage={userData.cuerrent_page}
-                                            perPage={perPage}
-                                            users={allUser}
-                                            total = {total}
-                                            paginate={paginate}
                                         />
-                                    ) : 'wait'}
 
-                                </div>
-                                <div className="d-flex justify-content-center">
-                                    {/*{!! $users->links('vendor.pagination.custom') !!}*/}
+                                        </tbody>
+                                    </table>
+                                    <div className="col-md-12">
+                                        {userData ? (
+                                            <Pagination
+                                                firstPageUrl={userData.first_page_url}
+                                                lastPageUrl={userData.last_page_url}
+                                                currentPage={userData.cuerrent_page}
+                                                perPage={perPage}
+                                                users={allUser}
+                                                total = {total}
+                                                paginate={paginate}
+                                            />
+                                        ) : 'wait'}
+
+                                    </div>
+                                    <div className="d-flex justify-content-center">
+                                        {/*{!! $users->links('vendor.pagination.custom') !!}*/}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </>
+
     )
 })
 
