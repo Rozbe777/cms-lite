da04001181bc340ead1f925cab6cae8363f6dc62
unknown <emran.rastadi@gmail.com>
2021-05-19 09:48:18 +0430

    update register regex

--- resources/views/panel/themes/frest/pages/admin/content/create.blade.php
+++ resources/views/panel/themes/frest/pages/admin/content/create.blade.php
@@ -5,17 +5,13 @@
 
 
 
-    <div class="content-wrapper" style="padding: 0px 0px !important;margin : 0px !important;">
+        <div class="content-wrapper" style="padding: 0px 0px !important;margin : 0px !important;">
+                <div id="form-creator" style="border-top : 1px solid #eee">
 
-
-        <div id="form-creator" style="border-top : 1px solid #eee">
+                </div>
 
         </div>
 
-
-
-
-    </div>
 
 
 
@@ -28,5 +24,5 @@
 
 
 
-    <script src="/js/app.js" ></script>
+    <script src="/js/app.js"></script>
 @endsection
