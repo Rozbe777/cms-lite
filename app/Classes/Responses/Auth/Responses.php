19873e9d043946ff330fab7791540565ef637985
mehrshad1367 <civilendarvish@gmail.com>
2021-05-20 01:11:47 +0430

    verify mobile table is Separated final

--- app/Classes/Responses/Auth/Responses.php
+++ app/Classes/Responses/Auth/Responses.php
@@ -8,29 +8,38 @@ class Responses
 {
     public function success($message, $data = [])
     {
-        return ($data != null) ?
-            response()->json([
-                "http_code" => 200,
-                "message" => $message,
-                "data" =>$data
-            ], 200) :
-            response()->json([
-                "http_code" => 200,
-                "message" => $message,
-            ], 200);
+        if (str_contains(\Route::current()->uri, 'api')) {
+            return ($data != null) ?
+                response()->json([
+                    "http_code" => 200,
+                    "message" => $message,
+                    "data" =>$data
+                ], 200) :
+                response()->json([
+                    "http_code" => 200,
+                    "message" => $message,
+                ], 200);
+        } else {
+            return adminView("pages.admin.dashboard.index");
+        }
     }
 
     public function notSuccess($message, $status, $data = [])
     {
-        return ($data != null) ?
-            response()->json([
-                "http_code" => $status,
-                "message" => $message,
-                "data" => $data
-            ], $status) :
-            response()->json([
-                "http_code" => $status,
-                "message" => $message,
-            ], $status);
+        if (str_contains(\Route::current()->uri, 'api')) {
+            return ($data != null) ?
+                response()->json([
+                    "http_code" => $status,
+                    "message" => $message,
+                    "data" => $data
+                ], $status) :
+                response()->json([
+                    "http_code" => $status,
+                    "message" => $message,
+                ], $status);
+        }else{
+            return adminView('pages.auth.login');
+        }
+
     }
 }
