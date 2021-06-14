d45a7ceb2e5c5510e3bbb1610d25f21821c06dfc
unknown <emran.rastadi@gmail.com>
2021-06-14 12:06:03 +0430

    product fetures

---
+++ resources/js/components/Admin/Helper/HelperClassFetures.js
@@ -0,0 +1,13 @@
+class HelperClassFetures {
+    constructor(data) {
+        this.data = data;
+    }
+    normalizees(){
+        return data;
+    }
+}
+
+export const Normalize = (data) => {
+    var Helpers = new HelperClassFetures(data)
+    Helpers.normalizees();
+}
