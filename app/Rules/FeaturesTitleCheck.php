19b0192d535538819e543130e7b4db29b3391005
Mehrshad <darvish1988.md@gmail.com>
2021-06-22 08:22:15 +0430

    products and Banks are going to test

--- app/Rules/FeaturesTitleCheck.php
+++ app/Rules/FeaturesTitleCheck.php
@@ -25,24 +25,27 @@ class FeaturesTitleCheck implements Rule
      */
     public function passes($attribute, $value)
     {
-        $i = 0;
-        $code = $value[0]['code'];
-        $code2 = $value[0]['code'];
-        do {
-            if (array_key_exists('color', $value[$i]))
-                $title['color'][] = $value[$i]['title'];
-            else
-                $title['text'][] = $value[$i]['title'];
+        if (!empty($value)) {
+            $i = 0;
+            $code = $value[0]['code'];
+            $code2 = $value[0]['code'];
+            do {
+                if (array_key_exists('color', $value[$i]))
+                    $title['color'][] = $value[$i]['title'];
+                else
+                    $title['text'][] = $value[$i]['title'];
 
-            $i++;
-            $code2 = $value[$i]['code'];
-        } while ($code == $code2);
+                $i++;
+                $code2 = $value[$i]['code'];
+            } while ($code == $code2);
 
-        if (
-            sizeof(array_count_values($title['text'])) != array_sum(array_count_values($title['text'])) ||
-            sizeof(array_count_values($title['color'])) != array_sum(array_count_values($title['color']))
-        ) {
-            return false;
+            if (
+                sizeof(array_count_values($title['text'])) != array_sum(array_count_values($title['text'])) ||
+                sizeof(array_count_values($title['color'])) != array_sum(array_count_values($title['color']))
+            ) {
+                return false;
+            }
+            return true;
         }
         return true;
     }
