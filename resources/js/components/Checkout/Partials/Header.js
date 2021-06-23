ed77563669fe0608ef7027d46f357403133dc8b9
unknown <emran.rastadi@gmail.com>
2021-06-23 13:36:17 +0430

    update

--- resources/js/components/Checkout/Partials/Header.js
+++ resources/js/components/Checkout/Partials/Header.js
@@ -10,7 +10,7 @@ export const Header = (props) => {
            <li><i className={"bx bx-cart"}></i> بررسی سبد خرید </li>
            <li><i className={"bx bx-truck"}></i> اطلاعات ارسال </li>
            <li><i className={"bx bx-wallet"}></i> نحوه پرداخت </li>
-           <li><i className={"bx bx-cart"}></i> پایان خرید </li>
+           <li><i className={"bx bx-check-circle"}></i> پایان خرید </li>
         </ul>
     )
 }
