c5297d9e720a83b0bdd6b50d591f1f5d7b836b3e
unknown <emran.rastadi@gmail.com>
2021-06-23 15:58:58 +0430

    update

--- resources/js/components/Checkout/index.js
+++ resources/js/components/Checkout/index.js
@@ -1,16 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {Header} from "./Partials/Header";
-
+import {ItemCheckOut} from './Partials/ItemCheckOut'
 const Index = () => {
     useEffect(() => {
     }, [])
     // const {mini} = props;
     const [state, setState] = useState();
     return (
-        <>
+        <div classNme={"row"} style={{padding : '20px'}}>
             <Header selected={"payType"} />
-        </>
+            <div classNme={"container-fluid"}>
+                <div classNme={"row"}>
+                    <ItemCheckOut />
+                </div>
+            </div>
+        </div>
     )
 }
 export default Index;
