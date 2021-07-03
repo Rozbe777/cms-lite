7457f52c83142ffa1ae89ee26636ff569981b397
mimshe <MSAHacker12>
2021-07-03 08:11:14 +0430

    theme setting initial

--- database/seeders/themes/parsa/parsa.php
+++ database/seeders/themes/parsa/parsa.php
@@ -16,5 +16,88 @@ return [
             'is_index' => 1,
             'published_at' => now(),
         ],
+    ],
+    'setting' => [
+        [
+            'name' => 'general',
+            'display_name' => 'general',
+            'status' => 'active',
+            'value' => [
+                [
+                    'name' => 'work_time',
+                    'display_name' => 'ساعت کاری',
+                    'type' => 'box',
+                    'visible' => 1,
+                    'value' => [
+                        'icon' => [
+                            'display_name' => 'آیکون',
+                            'type' => 'icon',
+                            'value' => 'ri-time-line',
+                            'visible' => 1,
+                        ],
+                        'title' => [
+                            'display_name' => 'عنوان',
+                            'type' => 'text',
+                            'value' => 'شنبه - جمعه: ',
+                            'visible' => 1,
+                        ],
+                        'description' => [
+                            'display_name' => 'توضیحات',
+                            'type' => 'text',
+                            'value' => '8:00 صبح - 9:00 عصر',
+                            'visible' => 1,
+                        ]
+                    ]
+                ],
+                [
+                    'name' => 'work_time',
+                    'display_name' => 'ساعت کاری',
+                    'type' => 'box',
+                    'visible' => 1,
+                    'value' => [
+                        'icon' => [
+                            'display_name' => 'آیکون',
+                            'type' => 'icon',
+                            'value' => 'ri-time-line',
+                        ],
+                        'title' => [
+                            'display_name' => 'عنوان',
+                            'type' => 'text',
+                            'value' => 'شنبه - جمعه: ',
+                        ],
+                        'description' => [
+                            'display_name' => 'توضیحات',
+                            'type' => 'text',
+                            'value' => '8:00 صبح - 9:00 عصر',
+                        ]
+                    ]
+                ],
+                [
+                    'name' => 'work_time',
+                    'display_name' => 'ساعت کاری',
+                    'type' => 'box',
+                    'visible' => 1,
+                    'value' => [
+                        'icon' => [
+                            'display_name' => 'آیکون',
+                            'type' => 'icon',
+                            'value' => 'ri-time-line',
+                        ],
+                        'title' => [
+                            'display_name' => 'عنوان',
+                            'type' => 'text',
+                            'value' => 'شنبه - جمعه: ',
+                        ],
+                        'description' => [
+                            'display_name' => 'توضیحات',
+                            'type' => 'text',
+                            'value' => '8:00 صبح - 9:00 عصر',
+                        ]
+                    ]
+                ]
+
+            ]
+        ]
+
     ]
 ];
