59e611035265743df527f5780878f77827e78944
Mehrshad <darvish1988.md@gmail.com>
2021-07-04 10:12:05 +0430

    category and contents refactor in image and Slug Handler

---
+++ app/Models/Repositories/Front/FrontCartProductsRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Models\Repositories\Front;
+
+
+class FrontCartProductsRepository implements \App\Models\Repositories\Admin\Interfaces\RepositoryInterface
+{
+
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function update(array $data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+}
