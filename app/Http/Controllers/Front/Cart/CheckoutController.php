5d31c871656766c75ab220404cb6428b7c7fd3e7
Mehrshad <darvish1988.md@gmail.com>
2021-07-13 09:23:40 +0430

    order with mohsen before coupon

--- app/Http/Controllers/Front/Cart/CheckoutController.php
+++ app/Http/Controllers/Front/Cart/CheckoutController.php
@@ -2,13 +2,47 @@
 
 namespace App\Http\Controllers\Front\Cart;
 
+use App\Classes\Responses\Front\ResponseTrait;
 use App\Http\Controllers\Controller;
+use App\Models\Attribute;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
-    public function index()
+    use ResponseTrait;
+    function blade(Request $request)
     {
-        return adminView('pages.admin.checkout');
+        if (!empty($request->input('id'))) {
+            $ids = (explode(',', $request->input('id')));
+            $attributes = Attribute::whereIn('product_id',$ids)->with('product')->with('typeFeatures')->get();
+            return frontView('checkout.index',compact('attributes'));
+        }
+        return $this->message(__('message.content.search.notSuccess'))->error();
+    }
+
+    public function getBySlug(Request $request)
+    {
+        if (!empty($request->input('slug'))) {
+            $products = Product::whereSlug($request->input('slug'))->active()->firstOrFail();
+            $attributes = Attribute::whereProductId($products->id)->with('product')->with('typeFeatures')->get();
+            return frontView('checkout.index',compact('attributes'));
+        }
+        return $this->message(__('message.content.search.notSuccess'))->error();
+    }
+
+    public function show(Request $request)
+    {
+        if (!empty($request->input('id'))) {
+            $ids = (explode(',', $request->input('id')));
+            $attributes = Attribute::whereIn('product_id',$ids)->with('product')->with('typeFeatures')->get();
+            return $this->data($attributes)->message(__('message.success.200'))->success();
+        }
+
+        if (!empty($request->input('slug'))) {
+            $products = Product::whereSlug($request->input('slug'))->active()->firstOrFail();
+            $attributes = Attribute::whereProductId($products->id)->with('product')->with('typeFeatures')->get();
+            return $this->message(__('message.success.200'))->data($attributes)->success();
+        }
     }
 }
