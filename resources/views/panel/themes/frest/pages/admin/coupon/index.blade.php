4a62d5acb8734972869b1a80e65214099069a658
unknown <emran.rastadi@gmail.com>
2021-07-13 08:02:22 +0430

    update

--- resources/views/panel/themes/frest/pages/admin/coupon/index.blade.php
+++ resources/views/panel/themes/frest/pages/admin/coupon/index.blade.php
@@ -1,1 +1,29 @@
-<?php
+@extends("panel.themes.frest.layouts.dashboardLayout")
+@php($title = "کوپن ها")
+
+@section("content")
+
+    <div class="container-fluid">
+        <div class="row">
+            <div id="discount-page" data-token="{{csrf_token()}}" style="width: 100% ; border-top: 1px solid #eee;"></div>
+        </div>
+    </div>
+
+@endsection
+@section('pageScripts')
+
+
+    <script>
+        // if (window.performance) {
+        //     console.info("window.performance works fine on this browser");
+        // }
+        // console.info(performance.navigation.type);
+        // if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+        //     console.info( "This page is reloaded" );
+        // } else {
+        //     console.info( "This page is not reloaded");
+        // }
+    </script>
+
+    <script src="/js/app.js"></script>
+@endsection
