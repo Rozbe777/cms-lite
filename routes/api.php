49912fd725457610f23d2905f848f2da9d79d582
mimshe <MSAHacker12>
2021-07-15 13:02:49 +0430

    api auth bug fixed!

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,28 @@ use App\Http\Controllers\Api\V1\Auth\PasswordController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('v1')->name('api.')->group(function () {
 
-Route::post('auth/login', [LoginController::class,'login'])->name('api.login');
-Route::get('logout', [LoginController::class,'logout'])->name('api.logout');
 
-Route::prefix('mobile/')->group(function(){
-    Route::post('register',[MobileRegisterController::class,'register'])->name('api.mobile.register');
-    Route::post('token', [MobileRegisterController::class, 'checkMobile'])->name('api.check.verification');
-});
-
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [RegisterController::class, 'store'])->name('api.auth.store');
-    Route::prefix('/password')->group(function () {
-        Route::post('/token', [PasswordController::class, 'passwordToken'])->name('api.auth.password.token');
-        Route::post('/recovery', [PasswordController::class, 'passwordRecovery'])->name('api.auth.password.recovery');
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::middleware('auth:api')->prefix('user')->group(function () {
+        Route::get('/', function () {
+            return auth()->user();
+        });
     });
-});
+    Route::prefix('mobile/')->group(function () {
+        Route::post('register', [MobileRegisterController::class, 'register'])->name('mobile.register');
+        Route::post('token', [MobileRegisterController::class, 'checkMobile'])->name('check.verification');
+    });
 
+    Route::prefix('auth/')->name('auth.')->group(function () {
+        Route::post('login', [LoginController::class, 'login'])->name('login');
+        Route::post('register', [RegisterController::class, 'store'])->name('store');
+        Route::prefix('password')->group(function () {
+            Route::post('token', [PasswordController::class, 'passwordToken'])->name('password.token');
+            Route::post('recovery', [PasswordController::class, 'passwordRecovery'])->name('password.recovery');
+        });
+    });
+
+});
 
